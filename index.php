f9c1bfc55b1822d50bafa3da2d1bda1a95cb23e4
CarRG <isc.carrg@gmail.com>
2018-12-02 22:01:47 -0600

    Ejemplo de JWT con PHP

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
-    $curl = curl_init();
+    echo "<pre>";
+
+    use Firebase\JWT\JWT;
+
+    require_once "JWT.php";
+
+    $time = time();
+    $key = "CarRG180818";
+    $token = array(
+        "username" => "CarRG",
+        "password" => "180818",
+        "iat" => $time, // Tiempo que inicia el token
+        "exp" => $time + (60 * 60) // Tiempo que expir el token, 1 hora
+    );
+
+    $jwt =JWT::encode($token, $key);
+    print_r($jwt);
+
+    echo "<br /><br />";
+
+    $decode_jwt = JWT::decode($jwt, $key, array("HS256"));
+    print_r($decode_jwt);
+
+    /* $curl = curl_init();
 
     curl_setopt($curl, CURLOPT_URL, "http://localhost:3000/v1/users");
 
@@ -12,14 +35,14 @@
         )
     );
 
-    /* curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_PUT, 1);s
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); 
-    curl_setopt($crl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, false); */
+    //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    //curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+    //curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    //curl_setopt($curl, CURLOPT_PUT, 1);s
+    //curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+    //curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); 
+    //curl_setopt($crl, CURLOPT_SSL_VERIFYHOST, false);
+    //curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, false);
 
     curl_setopt($curl, CURLOPT_POSTFIELDS, 
         '
@@ -40,8 +63,10 @@
     } 
     else
     {
-        echo $result;
+        print_r($result);
     }
 
-    curl_close($curl);
+    curl_close($curl); */
+
+    echo "</pre>"; 
 ?>
