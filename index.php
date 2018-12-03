dc2199140cf50b33ee9d224715f0bd3c39d92350
CarRG <isc.carrg@gmail.com>
2018-12-02 20:43:42 -0600

    Ejemplo cosumir api rest php con headers y body-data

--- index.php
+++ index.php
@@ -1,4 +1,47 @@
 <?php
-    echo "Hola mundo";
-    echo "Qué pedo?";
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_URL, "http://localhost:3000/v1/users");
+
+    curl_setopt($curl, CURLOPT_POST, 1);
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            //"Content-length: 0",
+            "Content-Type: application/json",
+            "Authorization: "
+        )
+    );
+
+    /* curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_PUT, 1);s
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); 
+    curl_setopt($crl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, false); */
+
+    curl_setopt($curl, CURLOPT_POSTFIELDS, 
+        '
+            {
+                "name": "Carmelo 3",
+                "last_name": "Bravo",
+                "phone": "7774412104"	
+            }
+        '
+    ); 
+
+    $result = curl_exec($curl);
+
+    if ($result === false)
+    {
+        // throw new Exception('Curl error: ' . curl_error($crl));
+        print_r('Curl error: ' . curl_error($curl));
+    } 
+    else
+    {
+        echo $result;
+    }
+
+    curl_close($curl);
 ?>
